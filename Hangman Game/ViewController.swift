aa1217870de70320fc40e786dc218b50bfdd836e
Ben <benclarke023@gmail.com>
2020-04-09 08:59:48 +0100

    Initial Commit

---
+++ Hangman Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hangman Game
+//
+//  Created by Ben Clarke on 09/04/2020.
+//  Copyright © 2020 Ben Clarke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
