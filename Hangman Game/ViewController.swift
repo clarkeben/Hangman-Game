6cb76384d57040cabca74a6407c9e11f5dae7ef3
Ben <benclarke023@gmail.com>
2020-04-13 20:50:09 +0100

    Set up initial project

--- Hangman Game/ViewController.swift
+++ Hangman Game/ViewController.swift
@@ -9,12 +9,75 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var wordLabel: UILabel!
+    @IBOutlet weak var guessesRemainingLabel: UILabel!
+    
+    var wordStrings = [String]()
+    var level = 1
+    var word = ""
+    var usedLetters = [String]()
+    var promtWord = ""
+    
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        title = "Hangman ๅ"
+        navigationController?.navigationBar.prefersLargeTitles =  true
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Clue", style: .plain, target: self, action: #selector(giveClue))
+        
+        loadLevel()
     }
-
-
+    
+    @objc func giveClue() {
+        // Create func to show one letter..
+        print("Clue tapped!")
+    }
+    
+    
+    @IBAction func letterTapped(_ sender: UIButton) {
+        guard let letter = sender.titleLabel?.text else { return }
+        usedLetters.append(letter)
+        checkLetter()
+    }
+    
+    func loadLevel() {
+        // Read data from disk on BG thread
+        if let fileURL = Bundle.main.url(forResource: "words", withExtension: "txt") {
+            if let wordContents = try? String(contentsOf: fileURL) {
+                var lines = wordContents.components(separatedBy: "\n")
+                lines.shuffle()
+                wordStrings += lines
+            }
+        }
+        
+        word = String(wordStrings[level]).trimmingCharacters(in: .whitespacesAndNewlines)
+        print(word)
+        checkLetter()
+    }
+    
+    func checkLetter() {
+        for letter in 0..<(word.count) {
+            let strLetter = String(letter)
+            
+            if usedLetters.contains(strLetter) {
+                promtWord += strLetter
+            } else {
+                promtWord += "?"
+            }
+        }
+        
+        wordLabel.text = promtWord
+    }
+    
+    
+    
 }
 
