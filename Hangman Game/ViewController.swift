8220a6dca196090fd5b88c77859b41dcac49dead
Ben <benclarke023@gmail.com>
2020-04-15 01:02:03 +0100

    Update Hangman game logic based on  whether it  word contains character

--- Hangman Game/ViewController.swift
+++ Hangman Game/ViewController.swift
@@ -14,15 +14,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var wordLabel: UILabel!
     @IBOutlet weak var guessesRemainingLabel: UILabel!
     
-    var wordStrings = [String]()
-    var level = 1
+    //  Array for all words + letters
+    var wordLetterArray = [String]()
     var word = ""
-    var usedLetters = [String]()
-    var promtWord = ""
     
+    var maskedWord = ""
+    var maskedWordArray = [String]()
+    
+    var level = 1
     var score = 0 {
         didSet {
             scoreLabel.text = "Score: \(score)"
+        }
+    }
+    
+    // Consider adding loading screen
+    var livesRemaining = 10 {
+        didSet {
+            guessesRemainingLabel.text = "\(livesRemaining) guesses left"
         }
     }
     
@@ -37,19 +46,43 @@ class ViewController: UIViewController {
     }
     
     @objc func giveClue() {
-        // Create func to show one letter..
+        
+        let randomElement = wordLetterArray.randomElement()?.capitalized
+        // Give element and make them loose a point!
         print("Clue tapped!")
     }
     
     
     @IBAction func letterTapped(_ sender: UIButton) {
-        guard let letter = sender.titleLabel?.text else { return }
-        usedLetters.append(letter)
-        checkLetter()
+        guard let letterChosen = sender.currentTitle?.lowercased() else { return }
+        
+        if wordLetterArray.contains(letterChosen) {
+            
+            for (index, letter) in wordLetterArray.enumerated() {
+                if letterChosen == letter {
+                    maskedWordArray[index] = letter
+                }
+            }
+            
+            maskedWord = maskedWordArray.joined()
+            score += 1
+            
+        } else {
+            score -= 1
+            livesRemaining -= 1
+        }
+        
+        sender.isEnabled = false
+        wordLabel.text = maskedWord
+        
+        // check to see if the game is completed + reset
+        checkToSeeIfCompleted()
     }
     
     func loadLevel() {
         // Read data from disk on BG thread
+        var wordStrings = [String]()
+        
         if let fileURL = Bundle.main.url(forResource: "words", withExtension: "txt") {
             if let wordContents = try? String(contentsOf: fileURL) {
                 var lines = wordContents.components(separatedBy: "\n")
@@ -58,26 +91,49 @@ class ViewController: UIViewController {
             }
         }
         
-        word = String(wordStrings[level]).trimmingCharacters(in: .whitespacesAndNewlines)
-        print(word)
-        checkLetter()
-    }
-    
-    func checkLetter() {
-        for letter in 0..<(word.count) {
-            let strLetter = String(letter)
-            
-            if usedLetters.contains(strLetter) {
-                promtWord += strLetter
-            } else {
-                promtWord += "?"
-            }
+        //  Save word into an array + string
+        word = wordStrings[level]
+        for letter in wordStrings[level] {
+            wordLetterArray.append(String(letter))
         }
         
-        wordLabel.text = promtWord
+        print(wordLetterArray)
+        print(word)
+        
+        for _ in 0..<wordLetterArray.count {
+            maskedWord += "?"
+            maskedWordArray.append("?")
+        }
+        
+        wordLabel.text = maskedWord
     }
     
+    
+    func checkToSeeIfCompleted() {
+        if livesRemaining > 0 {
+            
+            if maskedWord == word {
+              print("completed it mate")
+            }
+
+        } else {
+            print("Game ended")
+            // Restart by loading game again, or consider
+        }
+        
+        
+    }
+
+}
+
+extension ViewController {
+    
+    func acPopUpController(title: String, message: String, actionTitle: String = "OK") {
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
+        ac.addAction(UIAlertAction(title: actionTitle, style: .default, handler: nil)) // Consider adding a handler incase someone wants to restart
+        present(ac, animated: true)
+    }
     
     
 }
-
