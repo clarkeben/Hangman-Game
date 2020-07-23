75ae178cb678b7f0d3af85e1be58d9a1759daf98
Ben <benclarke023@gmail.com>
2020-07-24 00:34:00 +0100

    Add local notifications scheduled each week

--- Hangman Game/AppDelegate.swift
+++ Hangman Game/AppDelegate.swift
@@ -7,24 +7,70 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
     
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
         //print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last! as String)
         
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { (notifcationsAllowed, error) in
+            if error != nil {
+                print(error ?? "Error setting up notifications!!")
+            }
+        }
+        
+        scheduleNotification()
+        
+        setUpNavBarFormatting()
+        
+        return true
+    }
+    
+    func scheduleNotification() {
+        registerCategories()
+        
+        let center = UNUserNotificationCenter.current()
+        center.removeAllPendingNotificationRequests()
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Can you beat the hangman?"
+        content.body = "New words await you, player!"
+        content.categoryIdentifier = "reminder"
+        content.sound = .default
+        
+        var dateComponents = DateComponents()
+        dateComponents.hour = 9
+        dateComponents.minute = 30
+        dateComponents.weekday = 6
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        
+        let request = UNNotificationRequest(identifier: "weeklyAlert", content: content, trigger: trigger)
+        center.add(request)
+    }
+    
+    func registerCategories() {
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+        
+        let reminder = UNNotificationAction(identifier: "reminder", title: "Play", options: .foreground)
+        let dismiss = UNNotificationAction(identifier: "later", title: "Remind me later", options: .destructive)
+        let category = UNNotificationCategory(identifier: "reminder", actions: [reminder, dismiss], intentIdentifiers: [])
+        
+        center.setNotificationCategories([category])
+    }
+    
+    func setUpNavBarFormatting() {
         UINavigationBar.appearance().barTintColor = UIColor(named: K.Colours.bgColour)
         UINavigationBar.appearance().tintColor = UIColor(named: K.Colours.buttonColour)
         UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white,
                                                             NSAttributedString.Key.font: UIFont(name: K.Fonts.retroGaming, size: 22.0)!]
         UINavigationBar.appearance().isTranslucent = false
         UIBarButtonItem.appearance().setTitleTextAttributes([NSAttributedString.Key.font: UIFont(name: K.Fonts.retroGaming, size: 15.0)!], for: .normal)
-        
-        return true
     }
     
     // MARK: UISceneSession Lifecycle
